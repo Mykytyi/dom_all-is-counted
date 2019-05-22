1d36fefebc3443ba71ba2bda309fb5d8acb9b165
Nikita <cherevaty.nik@gmail.com>
2019-05-22 12:50:37 +0300

    'solution'

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,6 @@ function trackingClicks() {
   amountOfClicks++;
   localStorage.setItem('lcl_amountOfImg', amountOfClicks);
 }
+
+https://github.com/Mykytyi/dom_all-is-counted/commit/f837262b523918d0733ea96448be2018bfa6fd3e
+https://mykytyi.github.io/dom_all-is-counted/
